e5f06ed234552089f06919cfd0d50bb1ccd1011a
dirkarnez <smalldirkalex@gmail.com>
2023-05-04 12:58:05 +0800

    - revamp, add github.com/dirkarnez/excelize/v2 v2.7.2

--- main.go
+++ main.go
@@ -1,44 +1,50 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
-	"github.com/qax-os/excelize"
+	"github.com/xuri/excelize/v2"
 )
 
 func main() {
-	f := excelize.NewFile()
-
-	// Create a new sheet.
-	index := f.NewSheet("Sheet2")
-	// Set value of a cell.
-	f.SetCellValue("Sheet2", "A2", "Hello world.")
-	f.SetCellValue("Sheet1", "B2", 100)
-	// Set active sheet of the workbook.
-	f.SetActiveSheet(index)
-	// Save spreadsheet by the given path.
-	if err := f.SaveAs("Book1.xlsx"); err != nil {
-		log.Fatal(err)
-	}
-
-	f, err := excelize.OpenFile("Book1.xlsx")
+	src, err := excelize.OpenFile("Book1.xlsx")
 	if err != nil {
 		log.Fatal(err)
 	}
-	//defer f.Close()
+	defer f.Close()
 
-	// Get value from cell by given worksheet name and cell reference.
-	cell := f.GetCellValue("Sheet1", "B2")
-	fmt.Println(cell)
+	dst := excelize.NewFile()
+	src.CopySheetFrom()
 
-	// Get all the rows in the Sheet1.
-	rows := f.GetRows("Sheet1")
+	// // Create a new sheet.
+	// index := f.NewSheet("Sheet2")
+	// // Set value of a cell.
+	// f.SetCellValue("Sheet2", "A2", "Hello world.")
+	// f.SetCellValue("Sheet1", "B2", 100)
+	// // Set active sheet of the workbook.
+	// f.SetActiveSheet(index)
+	// // Save spreadsheet by the given path.
+	// if err := f.SaveAs("Book1.xlsx"); err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	for _, row := range rows {
-		for _, colCell := range row {
-			fmt.Print(colCell, "\t")
-		}
-		fmt.Println()
-	}
+	// f, err := excelize.OpenFile("Book1.xlsx")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// //defer f.Close()
+
+	// // Get value from cell by given worksheet name and cell reference.
+	// cell := f.GetCellValue("Sheet1", "B2")
+	// fmt.Println(cell)
+
+	// // Get all the rows in the Sheet1.
+	// rows := f.GetRows("Sheet1")
+
+	// for _, row := range rows {
+	// 	for _, colCell := range row {
+	// 		fmt.Print(colCell, "\t")
+	// 	}
+	// 	fmt.Println()
+	// }
 }
